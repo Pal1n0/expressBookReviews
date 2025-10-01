9bb29ca8dc1bbe93abea26b12e47bae54ba5c8aa
Pal1n0 <pavoldzama@gmail.com>
2025-10-01 10:58:52 -0400

    sesion authentication

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session && req.session.authorization) {
+    let token = req.session.authorization["accessToken"];
+    
+    // verify the token
+    jwt.verify(token, "access", (err, decoded) => {
+      if (!err) {
+        req.user = decoded;  // optionally attach decoded info
+        next();              // go to next route
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
 });
  
 const PORT =5000;
